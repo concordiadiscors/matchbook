48ec9137064d10b3578997e39673125477c8bead
Loïc Seigland <loic@loicseigland.ie>
2024-10-11 19:55:27 +0100

    Modify setup.py further

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-
+import os
+import re
 from setuptools import setup, find_packages
-from matchbook import __version__
+
+def read(*parts):
+    with open(os.path.join(os.path.dirname(__file__), *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(
     name="matchbook",
-    version=__version__,
+    version=find_version("matchbook", "__init__.py"),
     author="Rory Cole",
     author_email="rory.cole1990@gmail.com",
     description="Matchbook API Python wrapper",
     url="https://github.com/rozzac90/matchbook",
     packages=find_packages(),
     install_requires=[
-        "requests==2.32.3",
+        "requests==2.32.4",
         "python-dateutil==2.9.0",
         "pytz==2024.2"
     ],
