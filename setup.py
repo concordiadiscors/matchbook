6febc9b0281b5a260879e0438bcc0f4f834228fc
Loïc Seigland <loic@loicseigland.ie>
2024-10-11 19:57:13 +0100

    Might try with package versions which actually exist

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     url="https://github.com/rozzac90/matchbook",
     packages=find_packages(),
     install_requires=[
-        "requests==2.32.4",
+        "requests==2.32.3",
         "python-dateutil==2.9.0",
         "pytz==2024.2"
     ],
